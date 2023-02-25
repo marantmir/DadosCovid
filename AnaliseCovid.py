8eb626e85c1d9985324716a514b801ef26bdd9f6
Marco Antonio Miranda <marcoa.miranda@usp.br>
2023-02-24 22:58:29 -0300

    Add files via upload

--- AnaliseCovid.py
+++ AnaliseCovid.py
@@ -7,12 +7,14 @@ import streamlit as st
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
 # Melhorando o nome das colunas da tabela
-df = df.rename(columns = {'newDeaths': 'Novos Óbitos', 'newCases':'Novos Casos', 'deaths_per_100k_inhabitants':'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
+df = df.rename(columns={'newDeaths' : 'Novos Óbitos', 'newCases' : 'Novos Casos',
+                        'deaths_per_100k_inhabitants' : 'Óbitos por 100 mil habitantes',
+                        'totalCases_per_100k_inhabitants' : 'Casos por 100 mil habitantes'})
 
-df['date']
+
 
 # Gerando lista de estados
-estados = list(df['state'].unique())
+estados = list(df['state'].unique( ))
 
 # Seleção do estado
 state = st.sidebar.selectbox('Qual o Estado?', estados)
@@ -25,11 +27,12 @@ column = st.sidebar.selectbox('Qual o tipo de informação?', colunas)
 df = df[df['state'] == state]
 
 # Gerando o gráfico
-fig = px.line(df, x='date', y=column, title=column +' - '+ state)
-fig.update_layout(xaxis_title = 'Data', yaxis_title = column.upper(), title = {'x':0.5})
+fig = px.line(df, x='date', y=column, title=column + ' - ' + state)
+fig.update_layout(xaxis_title='Data', yaxis_title=column.upper( ), title={'x' : 0.5})
 
 st.title('DADOS COVID - BRASIL')
-st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu latera para escolher o estado desejado')
+st.write(
+    'Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu latera para escolher o estado desejado')
 
 st.plotly_chart(fig, use_container_width=True)
 
